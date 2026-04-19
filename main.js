dd2583a86eda9577d8162f3b1f6f21250cea2648
Sakshi M <amisadha0519@gmail.com>
2026-04-19 20:56:33 +0530

    fix: Delete online/offline eventListener main.js file

--- main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("offline", () => {
-  alert("You are offline. Cached content is being shown.");
-});
-
-window.addEventListener("online", () => {
-  alert("You are back online.");
-});
